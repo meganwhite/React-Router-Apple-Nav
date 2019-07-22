04328c96e7f5d6b8017cbd7ba530bc6ae2e7ced6
Megan White <meganwhite@Megans-MacBook-Pro.local>
2019-07-22 17:13:43 -0500

    added icons

--- apple-nav/src/components/NavWrapper.js
+++ apple-nav/src/components/NavWrapper.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+
+
 const NavWrapper = () => {
     return (
       <div>
         <div className="App">
-          <h1>React Router Mini</h1>
           <div>
-            <Link to="/">Home</Link>
+            <Link to="/"><img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg"/></Link>
           </div>
           <div>
-            <Link to="/about">About</Link>
+            <Link to="/">Mac</Link>
           </div>
           <div>
-            <Link to="/contact">Contact</Link>
+            <Link to="/about">iPad</Link>
+          </div>
+          <div>
+            <Link to="/contact">iPhone</Link>
+          </div>
+          <div>
+            <Link to="/contact">Watch</Link>
+          </div>
+          <div>
+            <Link to="/contact">TV</Link>
+          </div>
+          <div>
+            <Link to="/contact">Music</Link>
+          </div>
+          <div>
+            <Link to="/contact">Support</Link>
+          </div>
+          <div>
+            <Link to="/contact"><img src="https://image.flaticon.com/icons/svg/126/126474.svg"/></Link>
+          </div>
+          <div>
+            <Link to="/contact"><img src="https://image.flaticon.com/icons/svg/481/481382.svg"/></Link>
           </div>
         </div>
       </div>
